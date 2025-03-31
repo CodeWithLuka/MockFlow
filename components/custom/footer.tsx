9d81dfb0b7f6b70237892a45bd339b650775b8b5
Code With Luka Suave (CodeWithLuka) <lukasuave.nextai@gmail.com>
2025-03-31 15:11:41 +0200

    implement footer

---
+++ components/custom/footer.tsx
@@ -0,0 +1,14 @@
+export const Footer = () => {
+	return (
+		<footer className="bg-background py-4">
+			<div className="flex justify-between items-center container mx-auto px-4 text-center to-gray-200">
+				<p className="text-xs">
+					© 2025 ✧ EMMANUEL SENENGU ✧ All Rights Reserved.
+				</p>
+				<p className="text-xs hidden lg:flex">
+					A Website By ⚡ Luka Suave (Emmanuel Senengu) ⚡.
+				</p>
+			</div>
+		</footer>
+	);
+};
